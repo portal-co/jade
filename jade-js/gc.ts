7a7b90df257e6cd0b707859b4c34f232d3d30e1c
Graham Kelly <gkgoat6700@gmail.com>
2025-09-21 22:28:16 +0000

    gc

---
+++ jade-js/gc.ts
@@ -0,0 +1,141 @@
+const weakmapInternal = (a: object, WeakMap: typeof globalThis.WeakMap) =>
+  isPolyfillWeakMap(WeakMap)
+    ? ((a as any)[WeakMap.__symbol] ??=
+        "__create" in WeakMap && typeof WeakMap.__create === "function"
+          ? WeakMap.__create()
+          : {})
+    : undefined;
+const isPolyfillWeakMap = (
+  WeakMap: typeof globalThis.WeakMap
+): WeakMap is {
+  __symbol: string | symbol;
+  prototype: { __id: string };
+} & typeof globalThis.WeakMap =>
+  ("__symbol" in WeakMap &&
+    (typeof WeakMap.__symbol === "string" ||
+      typeof WeakMap.__symbol === "symbol")) as any;
+type GcState = {
+  [a: string]: { map: boolean; objs: any[]; objSet: WeakMap<any, {}> };
+};
+export class GCReactor {
+  #weakMap: typeof WeakMap;
+  #object: typeof Object;
+  #roots: any[] = [];
+  #rootSet: WeakMap<any, {}> | undefined = undefined;
+  #markSet: WeakMap<any, {}> | undefined = undefined;
+  #markObjs: any[] = [];
+  constructor({
+    WeakMap = globalThis.WeakMap,
+    Object = { ...globalThis.Object } as any,
+  }: {
+    WeakMap?: typeof globalThis.WeakMap;
+    Object?: typeof globalThis.Object;
+  } = {}) {
+    this.#weakMap = WeakMap;
+    this.#object = Object;
+  }
+  get gc() {
+    return () => this.#gc();
+  }
+  get root() {
+    return (a: any) => this.#root(a);
+  }
+  get unroot() {
+    return (a: any) => this.#unroot(a);
+  }
+  #root(a: any) {
+    if (typeof a !== "object" && typeof a !== "function") return;
+    if (!isPolyfillWeakMap(this.#weakMap)) return;
+    const rootSet = (this.#rootSet ??= new this.#weakMap());
+    if (rootSet.has(a)) return;
+    rootSet.set(a, {});
+    this.#roots = [a, ...this.#roots];
+  }
+  #unroot(a: any) {
+    if (typeof a !== "object" && typeof a !== "function") return;
+    if (!isPolyfillWeakMap(this.#weakMap)) return;
+    const rootSet = (this.#rootSet ??= new this.#weakMap());
+    rootSet.delete(a);
+    this.#roots = this.#roots.filter((a) => rootSet.has(a));
+  }
+  #gc() {
+    if (!isPolyfillWeakMap(this.#weakMap)) return;
+    const rootSet = (this.#rootSet ??= new this.#weakMap());
+    const roots = (this.#roots = this.#roots.filter((a) => rootSet.has(a)));
+    const state = {};
+    for (const rk in roots) this.#mark(roots[rk], state);
+    this.#sweep(state);
+  }
+  #mark(a: any, state: GcState) {
+    if (!isPolyfillWeakMap(this.#weakMap)) return;
+    const markSet = (this.#markSet ??= new this.#weakMap());
+    if (typeof a !== "object" && typeof a !== "function") {
+      const proto = this.#object.getPrototypeOf(a);
+      if (proto) this.#mark(proto, state);
+      return;
+    }
+    if (markSet.has(a)) return;
+    markSet.set(a, {});
+    this.#markObjs = [a, ...this.#markObjs];
+    const proto = this.#object.getPrototypeOf(a);
+    if (proto) this.#mark(proto, state);
+    if (proto === this.#weakMap.prototype) {
+      const r = (state[(a as any).__id] ??= {
+        map: false,
+        objs: [],
+        objSet: new this.#weakMap(),
+      });
+      r.map = true;
+      for (const obj in r.objs) {
+        this.#mark(r.objs[obj], state);
+      }
+    }
+    const d = this.#object.getOwnPropertyDescriptors(a);
+    for (const key in d) {
+      if (key === this.#weakMap.__symbol) {
+        for (var k in d.value)
+          if (k !== (markSet as any).__id) {
+            const r = (state[k] ??= {
+              map: false,
+              objs: [],
+              objSet: new this.#weakMap(),
+            });
+            if (!r.objSet.has(a)) {
+              r.objSet.set(a, {});
+              r.objs = [a, ...r.objs];
+            }
+            if (r.map) {
+              this.#mark((d.value as any)[k], state);
+            }
+          }
+      } else {
+        for (const k2 in { value: true, get: true, set: true })
+          if (k2 in d[key]) {
+            this.#mark((d[key] as any)[k2], state);
+          }
+      }
+    }
+  }
+  #sweep(state: GcState) {
+    if (!isPolyfillWeakMap(this.#weakMap)) return;
+    const markSet = (this.#markSet ??= new this.#weakMap());
+    for (var key in state) {
+      if (!state[key].map) {
+        for (const obj_key in state[key].objs) {
+          const obj = state[key].objs[obj_key];
+          if (!markSet.has(obj)) {
+            const val = weakmapInternal(obj, this.#weakMap);
+            if (val) {
+              delete val[key];
+              delete val[(state[key].objSet as any).__id];
+            }
+          }
+        }
+      }
+    }
+    for (const x of this.#markObjs) {
+      markSet.delete(x);
+    }
+    this.#markObjs = [];
+  }
+}
--- jade-js/index.ts
+++ jade-js/index.ts
@@ -1,6 +1,14 @@
-import { isPolyfillKey } from "@portal-solutions/semble-common"
+import { isPolyfillKey } from "@portal-solutions/semble-common";
+
 export function isCamoKey(a: any): boolean {
-    return a === "location" || a === "eval" || a === "parent" || a === "top" || a === "ActiveXObject";
+  return (
+    a === "location" ||
+    a === "eval" ||
+    a === "parent" ||
+    a === "top" ||
+    a === "ActiveXObject"
+  );
 }
-import * as _single_tenant from './single_tenant.ts'
-export const single_tenant = _single_tenant;
+import * as single_tenant from "./single_tenant.ts";
+
+export { isPolyfillKey, single_tenant };
